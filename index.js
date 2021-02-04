2852dc17e1f7806449b93b37302db21aa691d8b3
Darya Radzevich <ganocij1@gmail.com>
2021-02-04 17:18:51 +0300

    feat: add function

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
 http.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`);
 });
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/',(req,res) => {
-    res.sendFile(__dirname + '/index.html')
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-    console.log('connected')
-    socket.on('message', (msg) => {
-        socket.broadcast.emit('message', msg)
-    })
+io.on("connection", (socket) => {
+  console.log("connected");
+  socket.on("message", (msg) => {
+    socket.broadcast.emit("message", msg);
+  });
 });
