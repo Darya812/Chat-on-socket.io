c52b51ef4b539c31ac9f72d5e064a5e46180a2bd
Darya Radzevich <ganocij1@gmail.com>
2021-02-04 17:17:25 +0300

    feat: create server

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const app = require('express')();
-const http = require('http').Server(app);
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+http.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 });
 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + '/index.html')
+});
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-  });
-
-  io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      console.log('message: ' + msg);
-    });
-  });
-
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('connected')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
 });
---
+++ public/app.js
@@ -0,0 +1,50 @@
+const socket = io();
+
+let userName;
+let textarea = document.querySelector('#textarea');
+let messagePlace = document.querySelector('.message_place');
+do {
+  userName = prompt('Please ener your name: ');
+} while(!userName);
+
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter') {
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message) {
+    const msg = {
+        user: userName,
+        message: message.trim()
+    }
+    addMessage(msg,'outgoing');
+    textarea.value = '';
+    scrollToBottom();
+
+    socket.emit('message', msg);
+
+}
+
+function addMessage(user,type)  {
+    let elem = document.createElement('div');
+    let className = type;
+    elem.classList.add(className, 'message');
+
+    let messageContent = `
+    <h4>${user.user}</h4>
+    <p>${user.message}</p>`;
+
+    elem.innerHTML = messageContent;
+
+    messagePlace.appendChild(elem);
+}
+
+socket.on('message',(msg) => {
+    addMessage(msg, 'incoming')
+    scrollToBottom()
+});
+
+function scrollToBottom() {
+  messagePlace.scrollTop = messagePlace.scrollHeight
+};
